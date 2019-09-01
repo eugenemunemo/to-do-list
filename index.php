72cf7873ee599917b30df001f1b851c4969983cc
eugenemunemo <j.babybehindmusic@gmail.com>
2019-09-01 00:48:28 -0700

    JQUERY STARTS AND JQUERY MIN EXT
    JQUERY line throught  text decoration

--- index.php
+++ index.php
@@ -90,4 +90,41 @@ function displaylist(){
   
 }
 
+?>
+   
+</body>
+
+    <script>
+ // jquery start here
+$(document).ready(function(){
+   var item = 0;
+   $("li").each(function(c){
+       $(this).click(function(){
+       checked = c;
+        if(item == 0){
+           $(this).css("text-decoration", "line-through");
+           item = 1;
+           sessionStorage.setItem(checked, item);
+
+          }else{
+              $(this).css("text-decoration", "none");
+              item = 0;
+              sessionStorage.setItem(checked, item);
+           }
+       });
+   });
+$("li").each(function(c){
+   if(sessionStorage.getItem(c)==1){
+       $(this).css("text-decoration", "line-through");
+   }
+});
+});
+
+
+    </script>
+    <script src="js/main.js"></script>
+
+ 
+</html>
+
 
