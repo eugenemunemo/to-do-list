3c2557d4d08549e900f12f20bda0cf4f9add0e1c
eugenemunemo <j.babybehindmusic@gmail.com>
2019-09-01 01:03:17 -0700

    Update main.js
    click  close butto

--- js/main.js
+++ js/main.js
@@ -9,3 +9,12 @@ for (i = 0; i < myNodelist.length; i++) {
  myNodelist[i].appendChild(span);
 }
 
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
