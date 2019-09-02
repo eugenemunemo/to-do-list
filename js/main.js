7e345e7bc1c1b38b61d7dc15e2703a62435f2994
eugenemunemo <j.babybehindmusic@gmail.com>
2019-09-02 02:56:18 -0700

    css
    changes in java script and css

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ $("li").each(function(c){
   }
 });
 });
-
+z
 // Toggle between hiding and showing to do app replies/comments
 document.getElementById("myBtn").click();
 function myFunction(id) {
@@ -50,7 +50,7 @@ for (i = 0; i < myNodelist.length; i++) {
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
- myNodelist[i].appendChild(span);
+  myNodelist[i].appendChild(span);
 }
 
 // Click on a close button to hide the current list item
@@ -63,18 +63,26 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-//Create a new list item when clicking on the "Add" button
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
- if (inputValue === '') {
+  if (inputValue === '') {
     alert("You must write something!");
   } else {
-   document.getElementById("myUL").appendChild(li);
- }
- document.getElementById("myInput").value = "";
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
